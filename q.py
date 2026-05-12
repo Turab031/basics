aa6548a0a71a1f5b8fc626edaf7996a80811b6f8
Turab Manzoor <turabmanzoor032@gmail.com>
2026-05-12 12:12:38 +0530

    first commit

---
+++ q.py
@@ -0,0 +1,10 @@
+movies = []
+mov = input("enter 1st movie")
+mov2 = input("enter 1st movie")
+mov3 = input("enter 1st movie")
+
+
+movies.append(mov)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
